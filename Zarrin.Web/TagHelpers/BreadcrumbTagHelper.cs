378525552552df995db29b8ff667dfbc98e4d305
hoseinipeyrov <mir.hadi.peyrov@gmail.com>
2019-03-03 13:58:21 +0330

    BreadcrumbTagHelper problem fixed

---
+++ Zarrin.Web/TagHelpers/BreadcrumbTagHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace Zarrin.Web.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("bread-crumb")]
+    public class BreadcrumbTagHelper : TagHelper
+    {
+        [HtmlAttributeName("title")]
+        public string Title { get; set; }
+        [HtmlAttributeName("master-name")]
+        public string MasterTitle { get; set; }
+
+        [HtmlAttributeName("slave-name")]
+        public string SlaveTitle { get; set; }
+
+        [HtmlAttributeName("master-icon")]
+        public string MasterIcon { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string tag = $@"<section class='content-header'><h1>{Title}</h1>
+             <ol class='breadcrumb'>
+            <li><a href = '#' ><i class='fa fa-{MasterIcon}'></i>{MasterTitle}</a></li>
+            <li class='active'>{SlaveTitle}</li>
+        </ol>
+    </section>";
+
+
+            output.PreContent.SetHtmlContent(tag);
+        }
+    }
+}
