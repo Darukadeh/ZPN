bf9020902505f3de1c55a503f926445ea308401a
hadi <mir.hadi.peyrov@gmail.com>
2019-03-13 22:56:07 +0330

    clean code

---
+++ Zarrin.Web/Controllers/AccountController-asli.cs
@@ -0,0 +1,28 @@
+﻿namespace Zarrin.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Zarrin.DataAccess;
+    using Zarrin.Models.Entities;
+    using Zarrin.Services.Services;
+
+    public class AccountControllerasli : Controller
+    {
+        private readonly AccountService _accountService;
+        public AccountControllerasli(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        // GET: Users
+        public IActionResult Index()
+        {
+            var allUsers = _accountService.GetAllUsers();
+            return View(allUsers);
+        }
+
+         public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
